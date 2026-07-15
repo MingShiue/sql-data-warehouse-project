4f124f8aa935b99359ec0fadb1c358df814eb14a
蔡明學 <116322564+MingShiue@users.noreply.github.com>
2026-07-15 15:52:32 +0800

    Update insert_data.sql

--- scripts/insert_data.sql
+++ scripts/insert_data.sql
@@ -1,6 +1,9 @@
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+
 TRUNCATE TABLE bronze.crm_cust_info;
 BULK INSERT bronze.crm_cust_info
-FROM 'C:\datasets\source_crm\cust_info.csv'
+FROM 'C:\Users\acmin\Desktop\專案\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -9,7 +12,7 @@ WITH (
 
 TRUNCATE TABLE bronze.crm_prd_info;
 BULK INSERT bronze.crm_prd_info
-FROM 'C:\datasets\source_crm\prd_info.csv'
+FROM 'C:\Users\acmin\Desktop\專案\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 WITH(
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -18,7 +21,7 @@ WITH(
 
 TRUNCATE TABLE bronze.crm_sales_details;
 BULK INSERT bronze.crm_sales_details
-FROM 'C:\datasets\source_crm\sales_details.csv'
+FROM 'C:\Users\acmin\Desktop\專案\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -27,7 +30,7 @@ WITH (
 
 TRUNCATE TABLE bronze.erp_cust_az12;
 BULK INSERT bronze.erp_cust_az12
-FROM 'C:\datasets\source_erp\cust_az12.csv'
+FROM 'C:\Users\acmin\Desktop\專案\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 WITH(
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -36,7 +39,7 @@ WITH(
 
 TRUNCATE TABLE bronze.erp_loc_a101;
 BULK INSERT bronze.erp_loc_a101
-FROM 'C:\datasets\source_erp\loc_a101.csv'
+FROM 'C:\Users\acmin\Desktop\專案\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -45,9 +48,10 @@ WITH (
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\datasets\source_erp\px_cat_g1v2.csv'
+FROM 'C:\Users\acmin\Desktop\專案\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
 	TABLOCK
 );
+END
